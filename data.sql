4369a3d272776d93a9db383a743346b6cf26916e
ANAS ALSAMMARRAIE <90115863+anas599@users.noreply.github.com>
2023-03-27 18:46:13 +0300

    add queries && schema data

--- data.sql
+++ data.sql
@@ -1,5 +1,13 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(id, name, escape_attempts, neutered, weight_kg, date_of_birth) 
+VALUES (4, 'Devimon', 5, TRUE, 11, TO_DATE('May 12th, 2017', 'Mon DDth, YYYY'));
+
+INSERT INTO animals(id, name, escape_attempts, neutered, weight_kg, date_of_birth) 
+VALUES (3, 'Pikachu', 1, FALSE, 15.04, TO_DATE('Jan 7th, 2021', 'Mon DDth, YYYY'));
+
+INSERT INTO animals(id, name, escape_attempts, neutered, weight_kg, date_of_birth) 
+VALUES (2, 'Gabumon', 2, TRUE, 8, TO_DATE('Nov 15th, 2018', 'Mon DDth, YYYY'));
+
+INSERT INTO animals(id, name,escape_attempts, neutered, weight_kg, date_of_birth)
+VALUES (1, 'Agumon', 0, TRUE, 10.23,TO_DATE('Feb 3rd, 2020', 'Mon DDth, YYYY'));
