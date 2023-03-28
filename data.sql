34d52556b6275eaae1c464eaf54debcb7838b92c
anas599 <anas599@yahoo.com>
2023-03-28 19:25:40 +0300

    update negative weight

--- data.sql
+++ data.sql
@@ -11,16 +11,16 @@ INSERT INTO animals(id, name,escape_attempts, neutered, weight_kg, date_of_birth
 VALUES (1, 'Agumon', 0, TRUE, 10.23,TO_DATE('Feb 3rd, 2020', 'Mon DDth, YYYY'));
 
 INSERT INTO animals(species, id, name,escape_attempts, neutered, weight_kg, date_of_birth)
-VALUES ('Animal', 5, 'Charmander', 0, FALSE, 11,TO_DATE('Feb 8th, 2020', 'Mon DDth, YYYY'));
+VALUES ('Animal', 5, 'Charmander', 0, FALSE, -11,TO_DATE('Feb 8th, 2020', 'Mon DDth, YYYY'));
 
 INSERT INTO animals(species, id, name,escape_attempts, neutered, weight_kg, date_of_birth)
-VALUES ('Animal', 6, 'Plantmon', 0, TRUE, 5.7,TO_DATE('Nov 15th, 2021', 'Mon DDth, YYYY'));
+VALUES ('Animal', 6, 'Plantmon', 2, TRUE, -5.7,TO_DATE('Nov 15th, 2021', 'Mon DDth, YYYY'));
 
 INSERT INTO animals(species, id, name,escape_attempts, neutered, weight_kg, date_of_birth)
-VALUES ('Animal', 7, 'Squirtle', 3, FALSE, 12.13 ,TO_DATE('Apr 2nd, 1993', 'Mon DDth, YYYY'));
+VALUES ('Animal', 7, 'Squirtle', 3, FALSE, -12.13 ,TO_DATE('Apr 2nd, 1993', 'Mon DDth, YYYY'));
 
 INSERT INTO animals(species, id, name,escape_attempts, neutered, weight_kg, date_of_birth)
-VALUES ('Animal', 8, 'Angemon', 1, TRUE, 45 ,TO_DATE('Jun 12th, 2005', 'Mon DDth, YYYY'));
+VALUES ('Animal', 8, 'Angemon', 1, TRUE, -45 ,TO_DATE('Jun 12th, 2005', 'Mon DDth, YYYY'));
 
 INSERT INTO animals(species, id, name,escape_attempts, neutered, weight_kg, date_of_birth)
 VALUES ('Animal', 9, 'Boarmon', 7, TRUE, 20.4 ,TO_DATE('Jun 7th, 2005', 'Mon DDth, YYYY'));
