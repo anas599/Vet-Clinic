ef3f3f607ffb02f540cf7512ad81a86a6106eab7
anas599 <anas599@yahoo.com>
2023-04-04 20:19:39 +0300

    add index to tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -43,3 +43,23 @@ CREATE TABLE medical_treatment(
   medical_histories_id INTEGER REFERENCES medical_histories (id),
   treatments_id INTEGER REFERENCES treatments(id),
 );
+
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity INTEGER,
+  total_price DECIMAL,
+  invoice_id INTEGER REFERENCES invoices(id),
+  treatment_id INTEGER REFERENCES treatments(id)
+);
+
+--Add index to each table--
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
+CREATE INDEX idx_invoices_medical_history_id ON invoices (medical_history_id);
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories (patient_id);
+CREATE INDEX idx_medical_histories_status ON medical_histories (status);
+CREATE INDEX idx_patients_name ON patients (name);
+CREATE INDEX idx_treatments_medical_history_id ON treatments (medical_history_id);
+CREATE INDEX idx_treatments_type ON treatments (type);
+CREATE INDEX idx_treatments_name ON treatments (name);
